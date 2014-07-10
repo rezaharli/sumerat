2b324eb23059127b42ffad669e0e4154601ed78d
Reza Harli <reza.harli@gmail.com>
2014-07-10 13:07:53 +0700

    add form to input "petugas" into "surat tugas"

---
+++ application/views/admin/f_input_petugas.php
@@ -0,0 +1,54 @@
+<div class="navbar navbar-inverse">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Masukkan petugas yang ingin diutus</a>
+		</div>
+	</div><!-- /.container -->
+</div><!-- /.navbar -->
+<?php echo $this->session->flashdata("error");?>
+	
+<form action="<?=base_URL()?>surattugas/asdf/" method="post" accept-charset="utf-8">	
+
+	<table class="table-form" align="center" width="100%" border="5">
+		<tr>
+			<?php for ($i = 1; $i <= $jumlah_petugas; $i++):?>
+				<td width="33%" align="center">
+					<table class="table-form">
+						<tr>
+							<td width="30%">Nama Utusan</td>
+							<td>
+								<input class="form-control" style="width: 200px" type="text" name="nama_petugas[]" />
+							</td>
+						</tr>
+						<tr>
+							<td width="30%">NIP Utusan</td>
+							<td>
+								<input class="form-control" style="width: 200px" type="text" name="nip_petugas[]" />
+							</td>
+						</tr>
+						<tr>
+							<td width="30%">Jabatan Utusan</td>
+							<td>
+								<input class="form-control" style="width: 200px" type="text" name="jabatan_petugas[]" />
+							</td>
+						</tr>
+					</table>
+				</td>
+				<?php if($i % 3 == 0): ?>
+					<?php echo '</tr><tr>'; ?>
+				<?php endif ?>
+			<?php endfor;?>
+			<td width="33%" align="center">
+				<button type="submit" class="btn btn-primary">Simpan</button>
+				<a href="<?=base_URL()?>suratmasuk" class="btn btn-success">Kembali</a>
+			</td>
+		</tr>
+
+	</table>
+
+</form>
+
+<?php 
+
+/* End of file f_input_petugas.php */
+/* Location: ./application/views/admin/f_input_petugas.php */

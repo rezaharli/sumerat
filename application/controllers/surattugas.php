096ff3546dea43ca9426e1571db369950dc648d1
Reza Harli <reza.harli@gmail.com>
2014-07-10 13:14:15 +0700

    try to add method to print "surat tugas"

--- application/controllers/surattugas.php
+++ application/controllers/surattugas.php
@@ -7,7 +7,46 @@ class SuratTugas extends CI_Controller {
 		if ( ! $this->session->userdata('is_logged_in')) {
 			redirect('login');
 		}
-		$this->load->model('surat_tugas');
+		$this->load->model('surat_masuk');
+	}
+
+	function index() {
+		if($this->uri->segment(3)){
+			redirect('surattugas/inputpengutus/'.$this->uri->segment(3));
+		} else {
+			redirect('suratmasuk');
+		}
+	}
+
+	function form_process() {
+		$surat_tugas_data = $this->session->flashdata('surat_tugas_data');
+		if( ! empty($surat_tugas_data)){
+			$surat_tugas_data['nama_pengutus'] 		= addslashes($this->input->post('nama_pengutus'));
+			$surat_tugas_data['nip_pengutus'] 		= addslashes($this->input->post('nip_pengutus'));
+			$surat_tugas_data['jabatan_pengutus'] 	= addslashes($this->input->post('jabatan_pengutus'));
+			$surat_tugas_data['jumlah_petugas'] 	= addslashes($this->input->post('jumlah_petugas'));
+			$this->session->set_flashdata('surat_tugas_data', $surat_tugas_data);
+
+			$data['page'] 			= "f_input_petugas";
+			$data['jumlah_petugas'] = $surat_tugas_data['jumlah_petugas'];
+			$this->load->view('admin/aaa', $data);
+		} else {
+			redirect('suratmasuk');
+		}
+	}
+
+	function asdf() {
+		$petugas = $this->input->post('petugas');
+		echo "<pre>";
+		print_r($petugas);
+	}
+
+	function inputpengutus() {
+		$surat_tugas_data['id_surat_masuk']	= $this->uri->segment(3);
+		$this->session->set_flashdata('surat_tugas_data', $surat_tugas_data);
+		
+		$data['page'] = "f_input_pengutus";
+		$this->load->view('admin/aaa', $data);
 	}
 
 }

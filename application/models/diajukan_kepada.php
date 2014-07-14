e41dfcca33e0b61c207d6c6747f9bb9bef3e309f
Reza Harli <reza.harli@gmail.com>
2014-07-14 13:59:04 +0700

    add new file

---
+++ application/models/diajukan_kepada.php
@@ -0,0 +1,9 @@
+<?php
+class Diajukan_Kepada extends CI_Model {
+
+    var $table = 'diajukan_kepada';
+
+}
+
+/* End of file diajukan_kepada.php */
+/* Location: ./application/models/diajukan_kepada.php */

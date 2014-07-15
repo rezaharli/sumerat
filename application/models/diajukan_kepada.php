b769084f5f0ac8b1504dfa5abe732fdfd8e5663a
Reza Harli <reza.harli@gmail.com>
2014-07-15 11:07:05 +0700

    add new method
    add method to select "diajukan kepada" by "id surat masuk"

--- application/models/diajukan_kepada.php
+++ application/models/diajukan_kepada.php
@@ -3,6 +3,13 @@ class Diajukan_Kepada extends CI_Model {
 
     var $table = 'diajukan_kepada';
 
+    function select_by_id_surat_masuk($id_surat_masuk) {
+        $this->db->from($this->table);
+        $this->db->where('id_surat_masuk', $id_surat_masuk);
+        $query = $this->db->get();
+        return ($query->num_rows() > 0)  ? $query->result() : FALSE;
+    }
+
 }
 
 /* End of file diajukan_kepada.php */

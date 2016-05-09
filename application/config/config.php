84688a7cb4f9184ed33b833e5bcd94a3025c0bfa
Reza Harli <reza.harli@gmail.com>
2016-05-09 22:39:49 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://".$_SERVER['SERVER_NAME'].":8080/sumerat/";
+$config['base_url']	= "http://".$_SERVER['SERVER_NAME']."/sumerat/";
 
 /*
 |--------------------------------------------------------------------------
